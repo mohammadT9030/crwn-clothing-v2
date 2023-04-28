273965b3135cbd962e86d001a33e9a0acf07ff08
mohammadT9030 <torabi9030@gmail.com>
2023-04-28 17:48:55 +0330

    small import error fixed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { render } from "react-dom";
 import "./index.scss";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { CartProvider } from "./contexts.jsx/cart.context";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 
